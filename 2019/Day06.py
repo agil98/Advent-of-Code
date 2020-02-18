6d39b3462f403391fa8c174a71af5f10179e09de
Alex <alexandra.gil@hotmail.ca>
2020-02-17 19:11:27 -0800

    Added day 06 solution

---
+++ 2019/Day06.py
@@ -0,0 +1,39 @@
+from collections import defaultdict
+
+
+def bfs(graph, start, end=None):
+    dist = -1
+    seen = set()
+    frontier = [start]
+    while frontier:
+        new_frontier = []
+        for _ in range(len(frontier)):
+            p = frontier.pop()
+            for c in graph[p]:
+                if c == end:
+                    return dist
+                if c not in seen:
+                    new_frontier.append(c)
+                seen.add(c)
+
+        frontier = new_frontier
+        dist += 1
+
+    return dist
+
+
+orbits = defaultdict(set)
+undirected = defaultdict(set)
+planets = set()
+
+with open('Day06Input.txt', 'r') as fp:
+    for line in fp:
+        a, b = line.strip().split(')')
+        orbits[b].add(a)
+        undirected[b].add(a)
+        undirected[a].add(b)
+        planets.add(a)
+        planets.add(b)
+
+print ("Total orbits:", sum(bfs(orbits, p) for p in planets))
+print ("Minimum orbital transfers:", bfs(undirected, 'YOU', 'SAN'))
